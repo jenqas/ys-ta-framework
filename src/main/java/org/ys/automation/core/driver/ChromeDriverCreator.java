c916abd273c0bc1bb1a2d47a97dae1ac22b19b72
Yauheni Sidarovich <yauheni_sidarovich@epam.com>
2023-06-13 18:48:33 +0200

    Fixed chromedriver execution

--- src/main/java/org/ys/automation/core/driver/ChromeDriverCreator.java
+++ src/main/java/org/ys/automation/core/driver/ChromeDriverCreator.java
@@ -2,12 +2,15 @@ package org.ys.automation.core.driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class ChromeDriverCreator implements WebDriverCreator{
 
     @Override
     public WebDriver createWebDriver() {
         System.setProperty("webdriver.chrome.driver", RESOURCES_PATH + "chromedriver.exe");
-        return new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        return new ChromeDriver(options);
     }
 }
